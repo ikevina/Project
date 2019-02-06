42b378474f73354ccbdf912211a38d1477c37f07
justindooley <justindooley5@gmail.com>
2019-02-05 20:25:13 -0500

    Flask App

--- app.py
+++ app.py
@@ -1,54 +1,62 @@
-from flask import Flask, render_template, request, jsonify
 import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-import sqlite3 as sql
+import pandas as pd
+
+# Allow us to declare column types
+from sqlalchemy import Column, Integer, String, Float
+
+# Import Flask
+from flask import Flask, jsonify
+
+# PyMySQL 
+import pymysql
+pymysql.install_as_MySQLdb()
 
 #################################################
-
 # Database Setup
-
 #################################################
 
-engine = create_engine("sqlite:///FPA_FOD_20170508.sqlite")
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-session = Session(engine)
+# Create engine
+engine1 = create_engine("mysql://root:password@localhost/fires")
+
+# Query the db to Pandas
+data1 = pd.read_sql_query("SELECT * FROM fires", con = engine1)
+
+# Test Pandas_df
+print(data1.head())
+
+# Dataframe to JSON
+data_all1 = data1.to_json(orient='records')
+data_causes1 = data1["STAT_CAUSE_DESCR"].to_json(orient='columns')
 
 #################################################
-
 # Flask Setup
-
 #################################################
 
 app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
 
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/fires<br/>"
+        f"/api/v1.0/all<br/>"
+        f"/api/v1.0/causes<br/>"
     )
+@app.route("/api/v1.0/all")
+def names1():
+    return (data_all1)
 
-# all flask functions and returns should be here
-
-# Reference to the table
-fires = Base.classes.Fires
-
-@app.route("/api/v1.0/fires")
+@app.route("/api/v1.0/causes")
 def names():
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(fires.FIRE_NAME).all()
-
-    # Convert list of tuples into normal list
-    #  all_names = list(results)
-    return jsonify(results)
-   #  return jsonify(all_names)
-
-
+    return (data_causes1)
+    
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
